ab5f4843dee767aeb4385564b1cb7d78d3f6250a
Akzhol <tomikkz54@gmail.com>
2022-11-13 07:47:00 +0600

    counter of my sign in

---
+++ src/main/java/com/example/javaee/javaee/SessionServlet.java
@@ -0,0 +1,31 @@
+package com.example.javaee.javaee;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SessionServlet", value = "/SessionServlet")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+        if(count == null){
+            session.setAttribute("count",1);
+            count = 1;
+        }else {
+            session.setAttribute("count",count+1);
+        }
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Your count is: "+ count +"</h1>");
+        pw.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
